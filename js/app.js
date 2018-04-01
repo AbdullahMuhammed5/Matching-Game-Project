275d94cfa20e1b2dca3705da4adb7e33c4e58b47
AbdullahMuhammed5 <abdo56668@gmail.com>
2018-04-01 14:35:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,31 +5,6 @@
     End date : 
 */
 
-/*
- * Create a list that holds all of your cards. (✓)
- */
-
-
-/*
- * Display the cards on the page (✓)
- *   - shuffle the list of cards using the provided "shuffle" method below (✓)
- *   - loop through each card and create its HTML (✓)
- *   - add each card's HTML to the page (✓)
- */
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one) 
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 
 //............................... Runner/Controller Area .................//
 let OpenedCards = []; // contain two card to check if matched or not.
@@ -160,8 +135,12 @@ function matched(OpenedCards){
     for(let i = 0; i < OpenedCards.length; i++){
         $(OpenedCards[i]).addClass("match");
         $(OpenedCards[i]).addClass('correct');
+        
+        if($(OpenedCards[i]).hasClass('match')){
         listOfOpenedCards.push(OpenedCards[i]);
+        }
     }
+            
     if(listOfOpenedCards.length == 16){
         winFunction();
     }
@@ -232,6 +211,10 @@ function rateSystem(clicks){
     }
 }
 //__________________________________________________//
+
+/*
+ * Restart Game.
+ */
 
 function restart(){
     // remove all classes from each opened card.
